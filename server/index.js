c9f4f54d32aa07621405249df27606af08b1109e
Farhan Tanvir <farhantaanvir@gmail.com>
2023-03-27 17:50:14 +0600

    password strength

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ import userRoutes from "./routes/users.js"
 import postRoutes from "./routes/posts.js"
 import {register} from './controllers/auth.js'
 import {createPost} from './controllers/posts.js'
-import { verifyToken } from "./middleware/auth.js"
+import { verifyToken,checkStrength } from "./middleware/auth.js"
 import User from './models/User.js'
 import Post from "./models/Post.js"
 import {users,posts} from './data/index.js'
@@ -28,8 +28,8 @@ app.use(express.json())
 app.use(helmet())
 app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}))
 app.use(morgan('common'))
-app.use(bodyParser.json({limit:"30mb",extended:true}))
-app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
+app.use(bodyParser.json({limit:"15mb",extended:true}))
+app.use(bodyParser.urlencoded({limit:"15mb",extended:true}))
 app.use(cors())
 app.use("/assets",express.static(path.join(__dirname,'public/assets')))
 
@@ -43,10 +43,16 @@ const storage=multer.diskStorage({
         cb(null,file.originalname)
     }
 });
-const upload=multer({storage})
+const upload=multer({
+    storage,
+    fileFilter:(req,file,cb)=>{
+        if(req.body.password && !checkStrength(req.body.password).strong) cb(null,false);
+        else cb(null,true)
+    }
+})
 
 // routes with files
-app.post('/auth/register',upload.single('picture'), register )
+app.post('/auth/register',upload.single('picture'),register)
 app.post('/posts',verifyToken, upload.single('picture'),createPost)
 
 // routes
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -14,3 +14,14 @@ export const verifyToken = async(req,res,next)=>{
         res.status(500).json({error:err.message})
     }
 }
+
+export function checkStrength(pass){
+    if(pass.length<8) return {strong:false,msg:'Password must be of at least 8 characters'};
+    let x=false,y=false,z=false;
+    for(let s of pass){
+        if(s>='A' && s<='Z') x=true;
+        else if(s>='a' && s<='z') y=true;
+        else if(s>='0' && x<='9') z=true;
+    }
+    return {strong:(x&&y&&z),msg:`Must contain atleast 1 ${!x?'uppercase character':(!y?'lowercase character':'digit')}`}
+}
